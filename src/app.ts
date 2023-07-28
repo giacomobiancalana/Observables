cd309caec369ae4e65ca8c8a5693b0b1f257bcc6
giacomobiancalana <biancalanagiacomo20@gmail.com>
2023-07-28 21:38:49 +0200

    Observables introduction

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
+
+//TODO: vuoi provare a pubblicarlo??
+
 console.log("uuu");
+
+interval(1000).subscribe((uu) => console.log(uu));
+
+//TODO: da capire perchè non posso fare Observables.interval e come si importa per bene interval
