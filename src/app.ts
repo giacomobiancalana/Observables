00dfb43da1b312ba9271001b26218bcb658f9f3c
giacomobiancalana <biancalanagiacomo20@gmail.com>
2023-08-27 19:13:25 +0200

    Format (alt+shift+F) and extension for types (Twoslash Query Comments)

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,16 @@ import { interval } from 'rxjs';
 import { map, throttleTime } from 'rxjs/operators';
 
 let observable = interval(1000);
+//   ^?
 let observer = {
-  next: function(el: any) { console.log("value", el) }
+  next: function (el: any) { console.log("value", el) }
 };
 
 observable.pipe(
-map(function(val) {
-  return "t: " + val;
-}),
-throttleTime(2100)
+  map(function (val) {
+    return "t: " + val;
+  }),
+  throttleTime(2100)
 ).subscribe(observer);
 
 /*
